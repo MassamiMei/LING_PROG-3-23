14e6d67f5fe067108d54dd0f357899268fc81b5e
meiB) <106460099+MassamiMei@users.noreply.github.com>
2023-03-01 21:16:43 -0300

    exemplo2
    legal

---
+++ 3cti/1_intro/6_if_else.php
@@ -0,0 +1,18 @@
+<?php
+
+$dano = 520;
+$chance_item = 20000;
+$dano_critico = ($dano * 60)/100;
+$critico =  $chance_item * 2.5;
+
+if ($dano_critico < 100){
+
+echo 'fecha o gume logo seu otario kkkkkkkkkk';
+}
+else
+{
+	echo 'é mono kkkkkkkkkk';
+}
+
+ 
+?>
