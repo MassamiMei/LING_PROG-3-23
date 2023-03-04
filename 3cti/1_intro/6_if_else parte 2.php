797967ce75e0c7f209e73e9607080ddc3e698349
meiB) <106460099+MassamiMei@users.noreply.github.com>
2023-03-03 21:17:04 -0300

    :)

---
+++ 3cti/1_intro/6_if_else parte 2.php
@@ -0,0 +1,27 @@
+<?php
+
+$nome = "clara";
+
+if ($nome == "denicreidsson"){
+	echo "<hr> Bem vindo $nome " ;
+}
+else
+{
+	echo "<hr>Some daqui meu some daqui " ;
+
+}
+
+$a = 1;
+if ($a === '1'){
+	echo"<hr> A igual a 1";
+}
+else
+{
+	echo"<hr>a não é igual a 1";
+	
+}
+
+// =atribução
+// == comparação simples
+// === comparação indentica
+?>
